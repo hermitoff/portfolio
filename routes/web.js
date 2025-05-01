c9c30e17d2cb28dc4d6d68b0aee756d246c5fe45
Élodie GIMENEZ <perso@hermito.fr>
2025-05-01 17:46:48 +1100

    Ajout de la gestion dynamique des sections dans le CV

--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const cvdata = require('../data/cv.json');
+const renderSection = require('../utils/cv');
 
 // Route pour la page d'accueil
 router.get('/', (req, res) => {
 
     res.render('index', {  });
 });
-
+// Route pour la page du CV
 router.get('/cv', (req, res) => {
 
-    res.render('cv', {  });
+    res.render('cv', {
+        cvdata: cvdata,
+        renderSection
+      });
 });
 
 module.exports = router;
