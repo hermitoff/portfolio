8e0a5488c88268b5632b3282d4cbb0210efb5b8e
Élodie GIMENEZ <perso@hermito.fr>
2025-05-01 15:15:46 +1100

    Ajout d'un écran de chargement

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
-{}
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    const layout = document.querySelector('.cv-layout');
+    const sections = document.querySelectorAll('.cv-left, .cv-right');
+
+    loader.classList.add('hidden'); // Cache le loader
+
+    // Débloque les animations du contenu
+    layout.style.opacity = '1';
+    layout.style.animationPlayState = 'running';
+
+    sections.forEach(section => {
+      section.style.animationPlayState = 'running';
+      section.style.opacity = '1';
+    });
+});
