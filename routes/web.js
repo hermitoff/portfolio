34d81350e925f889bd7d0d63a789e46aaca366b7
Élodie GIMENEZ <perso@hermito.fr>
2025-07-10 09:34:33 +1100

    Supprimer la logique de rendu du CV et simplifier la route associée

--- routes/web.js
+++ routes/web.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const cvdata = require('../data/cv.json');
-const renderSection = require('../utils/cv');
 
 // Route pour la page d'accueil
 router.get('/', (req, res) => {
-
     res.render('index', {  });
 });
 // Route pour la page du CV
 router.get('/cv', (req, res) => {
-
-    res.render('cv', {
-        cvdata: cvdata,
-        renderSection
-    });
+    res.render('cv', {} );
 });
 
 module.exports = router;
