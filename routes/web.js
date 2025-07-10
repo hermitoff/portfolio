9d7e944671c81b699b5da62e2b0bc0b1c9803b15
Élodie GIMENEZ <perso@hermito.fr>
2025-07-10 19:21:39 +1100

    Fix navigation bar

--- routes/web.js
+++ routes/web.js
@@ -3,32 +3,32 @@ const router = express.Router();
 
 // Route pour la page d'accueil
 router.get('/', (req, res) => {
-    res.render('index', {  });
+    res.render('index', { currentPath: req.path });
 });
 
 // Route pour la page du CV
 router.get('/cv', (req, res) => {
-    res.render('cv', {} );
+    res.render('cv', { currentPath: req.path } );
 });
 
 // Route pour la page des projets
 router.get('/projets', (req, res) => {
-    res.render('projets', {} );
+    res.render('projets', { currentPath: req.path } );
 });
 
 // Route pour la page du parcours
 router.get('/parcours', (req, res) => {
-    res.render('parcours', {} );
+    res.render('parcours', { currentPath: req.path } );
 });
 
 // Route pour la page des compétences
 router.get('/competences', (req, res) => {
-    res.render('competences', {} );
+    res.render('competences', { currentPath: req.path } );
 });
 
 // Route pour la page de contact
 router.get('/contact', (req, res) => {
-    res.render('contact', {} );
+    res.render('contact', { currentPath: req.path } );
 });
 
 module.exports = router;
