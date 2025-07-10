c797c0f6f81db68a8f2d23cc23819041502e5bb5
Élodie GIMENEZ <perso@hermito.fr>
2025-07-10 13:39:06 +1100

    Fix CSS + pages placeholder

--- routes/web.js
+++ routes/web.js
@@ -5,9 +5,30 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index', {  });
 });
+
 // Route pour la page du CV
 router.get('/cv', (req, res) => {
     res.render('cv', {} );
+});
+
+// Route pour la page des projets
+router.get('/projets', (req, res) => {
+    res.render('projets', {} );
+});
+
+// Route pour la page du parcours
+router.get('/parcours', (req, res) => {
+    res.render('parcours', {} );
+});
+
+// Route pour la page des compétences
+router.get('/competences', (req, res) => {
+    res.render('competences', {} );
+});
+
+// Route pour la page de contact
+router.get('/contact', (req, res) => {
+    res.render('contact', {} );
 });
 
 module.exports = router;
